44150ed5ed50002440d12dd334ed64b9d5dd2e4c
westhnu <baha4001@hanyang.ac.kr>
2023-11-29 19:08:36 +0900

    Add SlidePuzzle_HSJ File[SlidePuzzle_HSJ]

---
+++ SlidePuzzle_HSJ/src/SlidePuzzle_HSJ/PuzzlePiece.java
@@ -0,0 +1,13 @@
+package SlidePuzzle_HSJ;
+
+class PuzzlePiece {
+    private int face;
+
+    public PuzzlePiece(int value) {
+        face = value;
+    }
+
+    public int face() {
+        return face;
+    }
+}
---
+++ SlidePuzzle_HSJ/src/SlidePuzzle_HSJ/PuzzleStarter.java
@@ -0,0 +1,8 @@
+package SlidePuzzle_HSJ;
+
+public class PuzzleStarter {
+
+    public static void main(String[] args) {
+        new PuzzleFrame(new SlidePuzzleBoard());
+    }
+}
